ec92adbc3bdf9d7971bb57572e729b484e0dcd6e
Caleb-Tan <calebtan23@gmail.com>
2018-02-20 14:50:11 -0800

    BST, randp, and mathset

--- eclipse-workspace/VocabularyGenerator/src/BST.java
+++ eclipse-workspace/VocabularyGenerator/src/BST.java
@@ -1,17 +1,34 @@
-class Driver {
+import java.util.ArrayList;
+import java.util.Collections;
+
+class DriverClass {
     public static void main(String args[]){
-        int[] arrNodes = {7, 3, 6, 1, 3, 10, 8, 11, 10};
-        BST<Integer> tree = new BST<>(arrNodes[0]);
-        for (int n: arrNodes){
-            BST<Integer> node = new BST<>(n);
-            tree.insert(node);
+        double depthAverage = 0;
+        for (int x = 0; x < 100; x++) {
+            ArrayList<Integer> arrNodes = new ArrayList<>();
+            for (int i = 1; i <= 10000; i++) arrNodes.add(i);
+            Collections.shuffle(arrNodes);
+            BST<Integer> tree = new BST<>(arrNodes.get(0));
+            for (int n : arrNodes) {
+                BST<Integer> node = new BST<>(n);
+                tree.insert(node);
+            }
+            depthAverage+=(double) tree.depth(tree);
         }
-        tree.printTree();
-        System.out.println();
-        System.out.println(tree.depth(tree));
+        depthAverage /= 100;
+        System.out.println(depthAverage);
     }
 }
 
+/*
+    The Average depth of a 10000 node tree inserted in random order is 31-32 levels deep. 
+    Algorithm Analysis:
+    Search worst case order of growth: O(n). The node is at the bottom of the tree, so all elements need to be traversed.
+    Search best case order of growth: O(1). The node to be searched is the root.
+    Search average case order of growth: O(log n), because a binary search's average order of growth is log n.
+    Insert average case order of growth: O(log n), insert method does basically the same thing as a search, except it inserts at a location below
+    Printing a tree order of growth: O(n), because you have to print all the elements.
+*/
 public class BST<T extends Comparable<T>> {
     private T datum;
     private BST<T> left;
@@ -49,6 +66,16 @@ public class BST<T extends Comparable<T>> {
         } else if (node.datum.compareTo(this.datum) < 0) {
             if (left == null) left = node;
             else left.insert(node);
+        }
+    }
+
+    protected boolean contains(BST<T> node){
+        if (node.datum.compareTo(this.datum) < 0) {
+            return left != null && left.contains(node);
+        } else if (node.datum.compareTo(this.datum) > 0) {
+            return right != null && right.contains(node);
+        } else {
+            return true;
         }
     }
 
--- eclipse-workspace/VocabularyGenerator/src/MathSet.java
+++ eclipse-workspace/VocabularyGenerator/src/MathSet.java
@@ -1,84 +1,184 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
-public class MathSet<E> implements Set<E>, Iterable<E> {
-    private E[] backingArray;
-    private int numElements;
-
+class Driver {
     public static void main(String args[]){
         MathSet<Integer> set1 = new MathSet<>();
         MathSet<Integer> set2 = new MathSet<>();
+        for (int i = 0; i < 10; i++){
+            set1.add((int) (Math.random()*50));
+            set2.add((int) (Math.random()*50));
+        }
+        System.out.println(Arrays.toString(set1.toArray()));
+        System.out.println(Arrays.toString(set2.toArray()));
+        MathSet<Integer> set1Copy = new MathSet<>();
+        MathSet<Integer> set2Copy = new MathSet<>();
+        set1Copy.addAll(set1);
+        set2Copy.addAll(set2);
+
+        set1.intersection(set2);
+        set1Copy.union(set2Copy);
+        System.out.println("Intersection of set1 and set2: " + Arrays.toString(set1.toArray()));
+        System.out.println("Union of set1 and set2: " + Arrays.toString(set1Copy.toArray()));
+
+    }
+}
+@SuppressWarnings("unchecked")
+public class MathSet<E> implements Set<E>, Iterable<E> {
+    private E[] internalArr;
+    private int numElements;
+
+
+    public MathSet() {
+        numElements = 0;
+        internalArr = (E[]) new Object[numElements];
     }
 
     /**
      * @param set - set to union with
      * @return
      */
-    public MathSet union(MathSet set) {
-        return set;
+    public void union(MathSet set) {
+        for (Object elem: set) {
+            if (!contains(elem)) {
+                add((E) elem);
+            }
+        }
+    }
+
+    /**
+     * finds intersection of two sets
+     * @param set
+     */
+    public void intersection(MathSet set) {
+        for (E elem: internalArr) {
+            if (!set.contains(elem)) {
+                remove(elem);
+                numElements--;
+            }
+        }
+    }
+    @Override
+    public boolean add(E e) {
+        if (contains(e)) return false;
+        numElements++;
+        E[] newArr=Arrays.copyOf(internalArr, numElements);
+        newArr[numElements-1] = e;
+        internalArr = newArr;
+        return true;
     }
 
     @Override
     public int size() {
-        return 0;
+        return numElements;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return numElements == 0;
     }
 
     @Override
     public boolean contains(Object o) {
+        for (E x: internalArr){
+            if (o != null && o == x){
+                return true;
+            }
+        }
         return false;
-    }
-
-    @Override
-    public Iterator iterator() {
-        return null;
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        Object[] obj = new Object[numElements];
+        for (int i = 0; i < internalArr.length; i++){
+            obj[i] = internalArr[i];
+        }
+        return obj;
     }
 
     @Override
-    public boolean add(Object o) {
-        return false;
+    public <T> T[] toArray(T[] a) throws ArrayStoreException, NullPointerException{
+        Object[] newArr = (Object[]) Array.newInstance(a.getClass(), numElements);
+        for (int i = 0; i < internalArr.length; i++){
+            newArr[i] = (T) internalArr[i];
+        }
+        return (T[]) newArr;
     }
 
     @Override
     public boolean remove(Object o) {
-        return false;
+        if (o == null) return false;
+        int x = 0;
+        boolean found = false;
+        E[] newArr = (E[]) new Object[numElements - 1];
+        for (E e : internalArr) {
+            if (e == o) found = true;
+            else {
+                newArr[x] = e;
+                x++;
+            }
+        }
+        if (found) {
+            internalArr = newArr;
+            return true;
+        } else return false;
     }
 
     @Override
-    public boolean addAll(Collection c) {
-        return false;
+    public boolean addAll(Collection<? extends E> c) {
+        for (E e: c) add(e);
+        return true;
     }
 
     @Override
     public void clear() {
-
+        numElements = 0;
+        E[] newArray = (E[])new Object[numElements];
+        internalArr = newArray;
     }
 
     @Override
     public boolean removeAll(Collection c) {
-        return false;
+        for (Object e: c) remove(e);
+        return true;
     }
 
     @Override
     public boolean retainAll(Collection c) {
-        return false;
+        boolean retained = false;
+        for (E e: internalArr) {
+            if (!c.contains(e)) {
+                remove(e);
+                retained = true;
+            }
+        }
+        return retained;
     }
 
     @Override
     public boolean containsAll(Collection c) {
-        return false;
+        for (Object e: c) {
+            if (!(contains(e))){
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
-    public Object[] toArray(Object[] a) {
-        return new Object[0];
+    public Iterator<E> iterator() {
+        Iterator<E> newItr = new Iterator<E>() {
+            private int currIdx = 0;
+            @Override
+            public boolean hasNext() {
+                return currIdx < numElements;
+            }
+            @Override
+            public E next() {
+                return internalArr[currIdx++];
+            }
+        };
+        return newItr;
     }
 }
